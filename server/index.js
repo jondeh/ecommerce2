5ab3c80e6f02caee4bd16313197917f5bf79203a
jondeh <dehlinjd@gmail.com>
2021-02-26 21:17:10 -0700

    first commit. built express server. built three basic functional components for the three main pages.

---
+++ server/index.js
@@ -0,0 +1,51 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
+const express = require("express"),
+    session = require("express-session"),
+    massive = require("massive"),
+    app = express(),
+    path = require('path'),
+    { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+// Controllers
+
+// const authCtrl = require("./controllers/authController");
+
+// Middleware
+app.use(express.json());
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+
+app.use(express.static(__dirname + '/../build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+}).then((db) => {
+  app.set("db", db)
+  app.listen(SERVER_PORT, () => {
+    console.log(`SERVER LISTENING ON ${SERVER_PORT}`)
+    console.log("DATABASE CONNECTED")
+});
+}).catch(err => console.log(err));
+
+
+
+// Auth Endpoints
+// app.post("/auth/register", authCtrl.register);
+// app.post("/auth/login", authCtrl.login);
+// app.post("/auth/logout", authCtrl.logout);
+// app.post("/auth/user", authCtrl.register);
+
+// User Endpoints
+// app.get("/api/users", userCtrl.getUsers);
---
+++ src/Components/Pages/MyPlan.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../../SCSS/LandingPage.scss';
+
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+
+        </div>
+    )
+}
+
+export default LandingPage;
