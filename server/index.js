c464f3afada648717b2148ef0fb01696f5b270a2
jondeh <dehlinjd@gmail.com>
2021-03-01 22:58:43 -0700

    looking for hosting bug

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config({ path: __dirname + "/../.env" });
 const express = require("express"),
-    test = require('../')
     session = require("express-session"),
     massive = require("massive"),
     app = express(),
@@ -24,7 +23,7 @@ app.use(
 );
 
 
-app.use(express.static(__dirname + '../build'));
+app.use(express.static(__dirname + '/../build'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'))
 });
