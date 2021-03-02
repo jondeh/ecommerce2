0a63442596c0bd30cfbdf23f95752b6ae48f27c1
jondeh <dehlinjd@gmail.com>
2021-03-02 00:00:44 -0700

    looking for hosted bug

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(
 
 
 app.use(express.static(__dirname + '/../build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 massive({
