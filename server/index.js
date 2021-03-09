4953eb533370716b4f21f48a86e203d05d0588aa
jondeh <dehlinjd@gmail.com>
2021-03-08 17:33:40 -0700

    edited server/index to try and fix production routing

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ app.use(
 );
 
 
-app.use(express.static(__dirname + '/../build'));
+// app.use(express.static(__dirname + '/../build'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
