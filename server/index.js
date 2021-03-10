c9e8dd0c5701f2bc42b6773d42255dbc69654aa4
jondeh <dehlinjd@gmail.com>
2021-03-10 11:25:54 -0700

    added a ../ before build in server index.js for routing bug

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
 // });
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   res.sendFile(path.join(__dirname, '../build', 'index.html'));
  });
 
 massive({
