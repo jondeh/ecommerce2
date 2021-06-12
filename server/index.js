9e996848945cfbb74bf3fc7d6cbe43da8cbc6a32
Jon Dehlin <jonde@pop-os.localdomain>
2021-06-11 22:11:35 -0600

    changed to __dirname on the server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-require("dotenv").config({ path: __dirname + "/../.env" });
-const express = require("express"),
-    cors = require("cors"),
-    session = require("express-session"),
-    massive = require("massive"),
-    app = express(),
-    path = require('path'),
-    stripe = require('stripe')(process.env.STRIPE_SECRET),
-    { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
+require('dotenv').config({ path: __dirname + '/../.env' })
+const express = require('express'),
+  cors = require('cors'),
+  session = require('express-session'),
+  massive = require('massive'),
+  app = express(),
+  path = require('path'),
+  stripe = require('stripe')(process.env.STRIPE_SECRET),
+  { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
 
 // Controllers
 
-const authCtrl = require("./controllers/authController");
-const payCtrl = require("./controllers/payController");
-const dashCtrl = require("./controllers/dashController");
+const authCtrl = require('./controllers/authController')
+const payCtrl = require('./controllers/payController')
+const dashCtrl = require('./controllers/dashController')
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 app.use(cors())
 
 app.use(
@@ -23,32 +23,31 @@ app.use(
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 * 4},
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 * 4 }
   })
-);
+)
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
-
+app.use(express.static(path.join(__dirname, '..', 'build')))
 
 massive({
   connectionString: CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-}).then((db) => {
-  app.set("db", db)
-  app.listen(SERVER_PORT, () => {
-    console.log(`SERVER LISTENING ON ${SERVER_PORT}`)
-    console.log("DATABASE CONNECTED")
-  });
-}).catch(err => console.log(err));
-
-
+  ssl: { rejectUnauthorized: false }
+})
+  .then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => {
+      console.log(`SERVER LISTENING ON ${SERVER_PORT}`)
+      console.log('DATABASE CONNECTED')
+    })
+  })
+  .catch(err => console.log(err))
 
 // Auth Endpoints
 
-app.post("/auth/register", authCtrl.register);
-app.post("/auth/login", authCtrl.login);
-app.post("/auth/logout", authCtrl.logout);
-app.post("/auth/user", authCtrl.register);
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.post('/auth/logout', authCtrl.logout)
+app.post('/auth/user', authCtrl.register)
 
 // User Endpoints
 // app.get("/api/users", userCtrl.getUsers);
@@ -57,16 +56,16 @@ app.post("/auth/user", authCtrl.register);
 
 // app.post('/pay', payCtrl.pay);
 // app.get('/secret', payCtrl.secret);
-app.post('/create-checkout-session', payCtrl.session);
+app.post('/create-checkout-session', payCtrl.session)
 
 // Dash Endpoints
 
-app.get('/get-day-weather/:lat/:lng', dashCtrl.getDayWeather);
-app.get('/get-open-weather/:lat/:lng', dashCtrl.getOpenWeather);
-app.get('/get-mosquitoes/:lat/:lng', dashCtrl.getMosquitoData);
-app.post('/get-prop-data', dashCtrl.getPropertyData);
+app.get('/get-day-weather/:lat/:lng', dashCtrl.getDayWeather)
+app.get('/get-open-weather/:lat/:lng', dashCtrl.getOpenWeather)
+app.get('/get-mosquitoes/:lat/:lng', dashCtrl.getMosquitoData)
+app.post('/get-prop-data', dashCtrl.getPropertyData)
 
 // todo add back in for production
 app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
- });
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+})
--- src/context/CustomContext.js
+++ src/context/CustomContext.js
@@ -1,170 +1,213 @@
-import React, { useState, createContext, useEffect, useContext } from "react";
-import { useLocation } from 'react-router-dom';
-import { states } from "../data/webData";
-import { UserContext } from '../context/UserContext';
-import axios from 'axios';
+import React, { useState, createContext, useEffect, useContext } from 'react'
+import { useLocation } from 'react-router-dom'
+import { states } from '../data/webData'
+import { UserContext } from '../context/UserContext'
+import axios from 'axios'
 
-export const CustomContext = createContext(null);
+export const CustomContext = createContext(null)
 export const CustomProvider = ({ children }) => {
-  const { register,  setUserLatLng} = useContext(UserContext);
+  const { register, setUserLatLng } = useContext(UserContext)
 
-  const location = useLocation();
-  const [whoAnswer, setWhoAnswer] = useState("no");
+  const location = useLocation()
+  const [whoAnswer, setWhoAnswer] = useState('no')
   const [whoPets, setWhoPets] = useState([])
-  const [homeAnswer, setHomeAnswer] = useState(null);
-  const [bugAnswer, setBugAnswer] = useState(null);
-  const [sprayerAnswer, setSprayerAnswer] = useState(null);
-  const [addressState, setAddressState] = useState(null);
-  const [addressCity, setAddressCity] = useState(null);
-  const [surveyNum, setSurveyNum] = useState(0);
-  const [farthestIndex, setFarthestIndex] = useState(0);
-  const [propData, setPropData] = useState(null);
-  const [homeLoad, setHomeLoad] = useState(false);
-  const [customLatLng, setCustomLatLng] = useState({lat: '', lng: ''});
-  const [address, setAddress] = useState("");
-  const [didAPICallFail, setDidAPICallFail] = useState(false);
-  const [perimeter, setPerimeter] = useState(0);
-  const [squareFeet, setSquareFeet] = useState(0);
-  const [sprayerInfo, setSprayerInfo] = useState(false);
-
-
+  const [homeAnswer, setHomeAnswer] = useState(null)
+  const [bugAnswer, setBugAnswer] = useState(null)
+  const [sprayerAnswer, setSprayerAnswer] = useState(null)
+  const [addressState, setAddressState] = useState(null)
+  const [addressCity, setAddressCity] = useState(null)
+  const [surveyNum, setSurveyNum] = useState(0)
+  const [farthestIndex, setFarthestIndex] = useState(0)
+  const [propData, setPropData] = useState(null)
+  const [homeLoad, setHomeLoad] = useState(false)
+  const [customLatLng, setCustomLatLng] = useState({ lat: '', lng: '' })
+  const [address, setAddress] = useState('')
+  const [didAPICallFail, setDidAPICallFail] = useState(false)
+  const [perimeter, setPerimeter] = useState(0)
+  const [squareFeet, setSquareFeet] = useState(0)
+  const [sprayerInfo, setSprayerInfo] = useState(false)
 
   useEffect(() => {
     if (surveyNum > farthestIndex) {
-      setFarthestIndex(surveyNum);
+      setFarthestIndex(surveyNum)
     }
   }, [surveyNum])
 
   useEffect(() => {
     if (homeAnswer) {
-        let homeAnswerArr = homeAnswer.split(',');
-        let homeState = homeAnswerArr[homeAnswerArr.length-2].trim().toLowerCase();
-        let homeCity = homeAnswerArr[homeAnswerArr.length-3].trim().toLowerCase();
-      setAddressState(homeState);
-      setAddressCity(homeCity);
-    };
-  }, [homeAnswer]);
+      let homeAnswerArr = homeAnswer.split(',')
+      let homeState = homeAnswerArr[homeAnswerArr.length - 2]
+        .trim()
+        .toLowerCase()
+      let homeCity = homeAnswerArr[homeAnswerArr.length - 3]
+        .trim()
+        .toLowerCase()
+      setAddressState(homeState)
+      setAddressCity(homeCity)
+    }
+  }, [homeAnswer])
 
   useEffect(() => {
     if (addressState) {
       if (states[addressState]) {
-        setBugAnswer(states[addressState]);
+        setBugAnswer(states[addressState])
       } else {
-        setBugAnswer(["ants", "spiders"]);
+        setBugAnswer(['ants', 'spiders'])
       }
     }
-  }, [addressState]);
+  }, [addressState])
 
-  const getPropData = (address) => {
-
+  const getPropData = address => {
     if (address) {
-      let homeAnswerArr = address.split(',');
-      let streetAddress = homeAnswerArr.shift();
-      homeAnswerArr.pop();
+      let homeAnswerArr = address.split(',')
+      let streetAddress = homeAnswerArr.shift()
+      homeAnswerArr.pop()
       let region = homeAnswerArr.join('')
-      axios.post(`/get-prop-data`, {address, streetAddress, region}).then(res => {
-        console.log("res: ", res)
-        let propData = res.data
-        setSquareFeet(Math.ceil(propData[0].sizeData.grossSize))
-        console.log("propData: ", propData[0])
-        setPerimeter(Math.ceil((Math.sqrt(propData[0].sizeData.groundFloorSize + propData[0].parkingData.prkgSize)*4)*1.1))
-        setHomeAnswer(address)
-        setPropData(propData)
-        setHomeLoad(false);
-        setDidAPICallFail(false);
-      })
-    };
-  };
-  
+      axios
+        .post(`/get-prop-data`, { address, streetAddress, region })
+        .then(res => {
+          console.log('res: ', res)
+          let propData = res.data
+          setSquareFeet(Math.ceil(propData[0].sizeData.grossSize))
+          console.log('propData: ', propData[0])
+          setPerimeter(
+            Math.ceil(
+              Math.sqrt(
+                propData[0].sizeData.groundFloorSize +
+                  propData[0].parkingData.prkgSize
+              ) *
+                4 *
+                1.1
+            )
+          )
+          setHomeAnswer(address)
+          setPropData(propData)
+          setHomeLoad(false)
+          setDidAPICallFail(false)
+        })
+    }
+  }
+
   useEffect(() => {
     if (address) {
-      setHomeLoad(true);
+      setHomeLoad(true)
       getPropData(address)
     }
-  }, [address]);
+  }, [address])
 
-  const addBug = (bug) => {
-    let newSuggestions = bugAnswer.map((e) => e);
+  const addBug = bug => {
+    let newSuggestions = bugAnswer.map(e => e)
 
     setBugAnswer(() => {
-      let bugIndex = newSuggestions.findIndex((e) => e === bug);
+      let bugIndex = newSuggestions.findIndex(e => e === bug)
 
       if (bugIndex === -1) {
-        return [...newSuggestions, bug];
+        return [...newSuggestions, bug]
       } else {
-        let finalSuggestions = [...newSuggestions];
-        finalSuggestions.splice(bugIndex, 1);
-        return [...finalSuggestions];
+        let finalSuggestions = [...newSuggestions]
+        finalSuggestions.splice(bugIndex, 1)
+        return [...finalSuggestions]
       }
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    setSurveyNum(0);
-    setFarthestIndex(0);
-    setWhoAnswer("no");
-    setWhoPets([]);
-    setHomeAnswer(null);
-    setBugAnswer(null);
-    setSprayerAnswer(null);
-    setAddressState(null);
-    setAddressCity(null);
-    setPropData(null);
-    setAddress("");
-  }, [location]);
+    setSurveyNum(0)
+    setFarthestIndex(0)
+    setWhoAnswer('no')
+    setWhoPets([])
+    setHomeAnswer(null)
+    setBugAnswer(null)
+    setSprayerAnswer(null)
+    setAddressState(null)
+    setAddressCity(null)
+    setPropData(null)
+    setAddress('')
+  }, [location])
 
-  const handlePetClick = (myPet) => {
-    let petsArr = [...whoPets];
-    let petIndex = petsArr.findIndex((e) => e === myPet)
+  const handlePetClick = myPet => {
+    let petsArr = [...whoPets]
+    let petIndex = petsArr.findIndex(e => e === myPet)
     if (petIndex === -1) {
-        petsArr.push(myPet)
+      petsArr.push(myPet)
     } else {
-        petsArr.splice(petIndex, 1)
+      petsArr.splice(petIndex, 1)
     }
 
-    setWhoPets(petsArr);
-};
-
-
-const handleNavClick = (index) => {
-  if (index <= farthestIndex) {
-      setSurveyNum(index);
+    setWhoPets(petsArr)
   }
-};
 
-const registerUser = ({email, password}, myplan) => {
-  register({email, password, whoAnswer, whoPets, homeAnswer, bugAnswer, sprayerAnswer, addressState, addressCity, address, perimeter, squareFeet, customLatLng }, myplan)
-}
+  const handleNavClick = index => {
+    if (index <= farthestIndex) {
+      setSurveyNum(index)
+    }
+  }
+
+  const registerUser = ({ email, password }, myplan) => {
+    register(
+      {
+        email,
+        password,
+        whoAnswer,
+        whoPets,
+        homeAnswer,
+        bugAnswer,
+        sprayerAnswer,
+        addressState,
+        addressCity,
+        address,
+        perimeter,
+        squareFeet,
+        customLatLng
+      },
+      myplan
+    )
+  }
 
   return (
     <CustomContext.Provider
       value={{
-        whoAnswer, setWhoAnswer,
-        whoPets, setWhoPets,
-        homeAnswer, setHomeAnswer,
-        bugAnswer, setBugAnswer,
-        sprayerAnswer, setSprayerAnswer,
-        addressState, setAddressState,
+        whoAnswer,
+        setWhoAnswer,
+        whoPets,
+        setWhoPets,
+        homeAnswer,
+        setHomeAnswer,
+        bugAnswer,
+        setBugAnswer,
+        sprayerAnswer,
+        setSprayerAnswer,
+        addressState,
+        setAddressState,
         addBug,
         handlePetClick,
-        addressCity, setAddressCity,
-        surveyNum, setSurveyNum,
-        farthestIndex, setFarthestIndex,
-        propData, setPropData,
+        addressCity,
+        setAddressCity,
+        surveyNum,
+        setSurveyNum,
+        farthestIndex,
+        setFarthestIndex,
+        propData,
+        setPropData,
         getPropData,
-        homeLoad, setHomeLoad,
-        customLatLng, setCustomLatLng,
-        address, setAddress,
+        homeLoad,
+        setHomeLoad,
+        customLatLng,
+        setCustomLatLng,
+        address,
+        setAddress,
         didAPICallFail,
-        perimeter, setPerimeter,
+        perimeter,
+        setPerimeter,
         handleNavClick,
-        sprayerInfo, setSprayerInfo,
-        squareFeet, setSquareFeet,
+        sprayerInfo,
+        setSprayerInfo,
+        squareFeet,
+        setSquareFeet,
         registerUser
-
       }}
     >
       {children}
     </CustomContext.Provider>
   )
-};
+}
